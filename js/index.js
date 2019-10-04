c04d685410819658b36d6a9b94102b205e571d65
vishalicious213 <vishalicious213@gmail.com>
2019-10-04 18:46:06 -0400

    bus image rotations

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 // Your code goes here
-// 1. mouseenter, 2. mouseleave
+// 1. mouseenter, 2. mouseleave, 3. mouseover, 4. click, 5. dblclick
 
 
-//HEADER
+// HEADER
 let logoHeading = document.querySelector(".logo-heading");
 // console.log(logoHeading);
 logoHeading.addEventListener("mouseenter", (event) => {logoHeading.style.backgroundColor = "red"});
 logoHeading.addEventListener("mouseleave", (event) => {logoHeading.style.backgroundColor = "white"});
 
 let navLink = document.querySelectorAll(".nav-link");
-console.log(navLink);
+// console.log(navLink);
 navLink.forEach(item => {
     item.addEventListener("mouseover", (event) => {
         item.style.fontSize = "250%"
     })
 })
-// navLink.addEventListener("mouseover", (event) => {navLink.style.fontSize = "250%"});
 
-// const navLinkMove = document.querySelectorAll('.nav-link');
-// navLinkMove.forEach(item =>{
-//     item.addEventListener("mouseleave", e=>{
-//         console.log(`mouse has left`);
-//         item.style.color = 'black';
-//     })
-// })
+// CONTAINER
+let busImg = document.querySelector(".home img");
+// console.log(busImg);
+busImg.addEventListener("click", (event) => {busImg.style.transform = "rotate(45deg)"});
+busImg.addEventListener("dblclick", (event) => {busImg.style.transform = "rotate(-45deg)"});
+
+let letsGoImg = document.querySelector(".content-section img");
+console.log(letsGoImg);
